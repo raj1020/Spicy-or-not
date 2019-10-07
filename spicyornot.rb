ea42af7c57aab67a4ddf4a67bec636af8e380999
raj1020 <raj.1020@hotmail.com>
2019-10-07 14:32:29 +1100

    defined method to pay for spicy and not spicy food

---
+++ spicyornot.rb
@@ -0,0 +1,26 @@
+def spicy_food(spice_arr, price_arr)
+    #Your code here
+    i=0
+    new_arr = []
+    spice = 0
+    no_spice = 0
+    while i<spice_arr.count
+        if spice_arr[i] == "S"
+            spice += price_arr[i]
+        else
+            no_spice += price_arr[i]
+        end
+
+
+    end
+    new_arr.push(spice)
+    new_arr.push(no_spice)
+    return new_arr
+
+end
+
+
+p spicy_food(['S', 'N', 'N', 'S'], [12, 18, 17, 15])
+p spicy_food(['N'], [23])
+p spicy_food(['S', 'S', 'S', 'S', 'N'], [15, 14, 19, 23, 20])
+p spicy_food([], [])
